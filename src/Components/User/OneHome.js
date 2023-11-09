e53598ffc5e7594857889c0e72e1e3e8a69b89c7
David <david.ndiba@student.moringaschool.com>
2023-11-09 20:49:27 +0300

    changed the frontend layout

--- src/Components/User/OneHome.js
+++ src/Components/User/OneHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useParams,  useNavigate } from 'react-router-dom';
-import building from "../../assets/images/building_homes.jpg";
+import { useParams, useNavigate } from 'react-router-dom';
+import building from '../../assets/images/building_homes.jpg';
 
 const OneHome = () => {
   const { id } = useParams();
@@ -15,49 +15,122 @@ const OneHome = () => {
       visitDate: '', // Property to hold set visit date
       donationMade: false, // Property to track if a donation has been made
     },
-
+    // Add more home data as needed
   ];
-
 
   const selectedHome = homeData.find((home) => home.id === parseInt(id));
 
   const handleSetVisitDate = (homeId) => {
     // Logic to set visit date for the selected home
     console.log(`Set visit date for home ${homeId}`);
-    history('/schedule_visit')
-  
+    history('/schedule_visit');
   };
 
   const handleMakeDonation = (homeId) => {
     // Logic to make a donation for the selected home
     console.log(`Make donation for home ${homeId}`);
-    history('/donate')
-    
+    history('/donate');
   };
 
   return (
-    <div className="flex max-w-sm rounded shadow-lg m-4">
-    {selectedHome && (
-      <>
-        <img src={selectedHome.image} alt={selectedHome.name} className="w-1/3" />
-        <div className="flex flex-col justify-between p-4 w-2/3">
-          <h2>{selectedHome.name}</h2>
-          <p>{selectedHome.description}</p>
-          {!selectedHome.visitDate ? (
-            <button onClick={() => handleSetVisitDate(selectedHome.id)} style={{ backgroundColor: '#E57C23', color:' #F8F1F1' }}>Set Visit Date</button>
-          ) : (
-            <p>Visit Date: {selectedHome.visitDate}</p>
-          )}
-          {!selectedHome.donationMade ? (
-            <button onClick={() => handleMakeDonation(selectedHome.id)} style={{ backgroundColor: '#E57C23', color:' #F8F1F1' }}>Make Donation</button>
-          ) : (
-            <p>Donation Made</p>
-          )}
+    <div className="container mx-auto mt-16 p-4 max-w-2xl">
+      {selectedHome && (
+        <div className="flex gap-8">
+          <div className="flex-shrink-0 w-1/2">
+            <img src={selectedHome.image} alt={selectedHome.name} className="w-full h-auto" />
+          </div>
+          <div className="flex-grow">
+            <h2 className="text-3xl font-semibold mb-4">{selectedHome.name}</h2>
+            <p className="text-gray-700">{selectedHome.description}</p>
+            {!selectedHome.visitDate ? (
+              <button
+                onClick={() => handleSetVisitDate(selectedHome.id)}
+                className="mt-4 px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 focus:outline-none focus:ring focus:border-blue-300"
+              >
+                Set Visit Date
+              </button>
+            ) : (
+              <p className="mt-4">Visit Date: {selectedHome.visitDate}</p>
+            )}
+            {!selectedHome.donationMade ? (
+              <button
+                onClick={() => handleMakeDonation(selectedHome.id)}
+                className="mt-4 px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 focus:outline-none focus:ring focus:border-blue-300"
+              >
+                Make Donation
+              </button>
+            ) : (
+              <p className="mt-4">Donation Made</p>
+            )}
+          </div>
         </div>
-      </>
-    )}
-  </div>
+      )}
+    </div>
   );
 };
 
 export default OneHome;
+
+// import React from 'react';
+// import { useParams,  useNavigate } from 'react-router-dom';
+// import building from "../../assets/images/building_homes.jpg";
+
+// const OneHome = () => {
+//   const { id } = useParams();
+//   const history = useNavigate();
+
+//   const homeData = [
+//     {
+//       id: 1,
+//       name: 'Cozy Cottage',
+//       image: building,
+//       description: 'A charming cottage in a peaceful location.',
+//       visitDate: '', // Property to hold set visit date
+//       donationMade: false, // Property to track if a donation has been made
+//     },
+
+//   ];
+
+
+//   const selectedHome = homeData.find((home) => home.id === parseInt(id));
+
+//   const handleSetVisitDate = (homeId) => {
+//     // Logic to set visit date for the selected home
+//     console.log(`Set visit date for home ${homeId}`);
+//     history('/schedule_visit')
+  
+//   };
+
+//   const handleMakeDonation = (homeId) => {
+//     // Logic to make a donation for the selected home
+//     console.log(`Make donation for home ${homeId}`);
+//     history('/donate')
+    
+//   };
+
+//   return (
+//     <div className="flex max-w-sm rounded shadow-lg m-4">
+//     {selectedHome && (
+//       <>
+//         <img src={selectedHome.image} alt={selectedHome.name} className="w-1/3" />
+//         <div className="flex flex-col justify-between p-4 w-2/3">
+//           <h2>{selectedHome.name}</h2>
+//           <p>{selectedHome.description}</p>
+//           {!selectedHome.visitDate ? (
+//             <button onClick={() => handleSetVisitDate(selectedHome.id)} style={{ backgroundColor: '#E57C23', color:' #F8F1F1' }}>Set Visit Date</button>
+//           ) : (
+//             <p>Visit Date: {selectedHome.visitDate}</p>
+//           )}
+//           {!selectedHome.donationMade ? (
+//             <button onClick={() => handleMakeDonation(selectedHome.id)} style={{ backgroundColor: '#E57C23', color:' #F8F1F1' }}>Make Donation</button>
+//           ) : (
+//             <p>Donation Made</p>
+//           )}
+//         </div>
+//       </>
+//     )}
+//   </div>
+//   );
+// };
+
+// export default OneHome;
--- src/Components/User/Visit.js
+++ src/Components/User/Visit.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const VisitComponent = () => {
-
+const VisitComponent = ({ isLoggedIn }) => {
   const visits = [
     { id: 1, date: '2023-11-05', home: 'Home A', visitor: 'John Doe' },
     { id: 2, date: '2023-11-06', home: 'Home B', visitor: 'Jane Smith' },
-  
   ];
 
   return (
@@ -18,8 +16,39 @@ const VisitComponent = () => {
           <p>Visitor: {visit.visitor}</p>
         </div>
       ))}
+      {!isLoggedIn && (
+        <div className="col-span-3 text-center mt-4 text-red-500">
+          Please log in to see and set visit dates.
+        </div>
+      )}
     </div>
   );
 };
 
 export default VisitComponent;
+
+// import React from 'react';
+
+// const VisitComponent = () => {
+
+//   const visits = [
+//     { id: 1, date: '2023-11-05', home: 'Home A', visitor: 'John Doe' },
+//     { id: 2, date: '2023-11-06', home: 'Home B', visitor: 'Jane Smith' },
+  
+//   ];
+
+//   return (
+//     <div className="grid grid-cols-3 gap-4 mb-10 mt-10 ml-6">
+//       {visits.map((visit) => (
+//         <div key={visit.id} className="bg-white p-4 rounded-md shadow-md">
+//           <p className="font-bold mb-2">Visit {visit.id} </p>
+//           <p className="mb-2">Date: {visit.date}</p>
+//           <p className="mb-2">Home: {visit.home}</p>
+//           <p>Visitor: {visit.visitor}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default VisitComponent;
